f8c005ab46c9fff7c9bb5c3c3a9c57c71e050f0c
devingrischow <devingrischow@outlook.com>
2025-09-16 20:00:09 -0500

    Minor-Change: Created basic layout for macOS widget, mirroring ios's widget

--- examples/simple_apple_widget/macos/simple_macos_widget/simple_macos_widget.swift
+++ examples/simple_apple_widget/macos/simple_macos_widget/simple_macos_widget.swift
@@ -1,13 +1,12 @@
 //
-//  simple_ios_flutter_widget.swift
-//  simple_ios_flutter_widget
+//  simple_macos_widget.swift
+//  simple_macos_widget
 //
 //  Created by Devin Grischow on 9/16/25.
 //
 
 import WidgetKit
 import SwiftUI
-import home_widget
 
 private let widgetGroupId = "group.example.widget_group"
 let countKey = "count";
@@ -16,15 +15,12 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), count: "0")
     }
-    
+
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let prefs = UserDefaults.init(suiteName: widgetGroupId)
-        let counterValue = prefs?.string(forKey: countKey) ?? "0"
-        
-        let entry = SimpleEntry(date: Date(), count: counterValue)
+        let entry = SimpleEntry(date: Date(), count: "0")
         completion(entry)
     }
-    
+
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         
         getSnapshot(in: context) { (entry) in
@@ -33,6 +29,10 @@ struct Provider: TimelineProvider {
         }
         
     }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -40,45 +40,29 @@ struct SimpleEntry: TimelineEntry {
     let count: String
 }
 
-struct simple_ios_flutter_widgetEntryView : View {
+struct simple_macos_widgetEntryView : View {
     var entry: Provider.Entry
-    
+
     var body: some View {
         VStack {
             Text("Pressed")
             Text(entry.count)
             Text("Times")
-            
-            if #available(iOSApplicationExtension 17, *){
-                //Big Iteractible Button
-                Button(intent:  BackgroundIntent(
-                    url: URL(string: "homeWidgetExample://incrementPressed"),
-                    appGroup: widgetGroupId)
-                ){
-                    Text("+")
-                }
-            }
+
             
         }
     }
 }
 
-struct simple_ios_flutter_widget: Widget {
-    let kind: String = "simple_ios_flutter_widget"
-    
+struct simple_macos_widget: Widget {
+    let kind: String = "simple_macos_widget"
+
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            simple_ios_flutter_widgetEntryView(entry: entry)
+            simple_macos_widgetEntryView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
     }
 }
-
-//#Preview(as: .systemSmall) {
-//    simple_ios_flutter_widget()
-//} timeline: {
-//    SimpleEntry(date: .now, emoji: "😀")
-//    SimpleEntry(date: .now, emoji: "🤩")
-//}
---
+++ examples/simple_apple_widget/macos/simple_macos_widget/simple_macos_widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  simple_macos_widgetBundle.swift
+//  simple_macos_widget
+//
+//  Created by Devin Grischow on 9/16/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct simple_macos_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        simple_macos_widget()
+    }
+}

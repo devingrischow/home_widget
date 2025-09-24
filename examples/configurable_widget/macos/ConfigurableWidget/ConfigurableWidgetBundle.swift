ece31fac0aaf6598b4fa685eb5abbfb42c176f98
devingrischow <devingrischow@outlook.com>
2025-09-24 18:03:20 -0500

    MAJOR-Minor: Modern Configure Widgets works for macOS Will Continue to test for preformance. Currently testing legacy app intent, but presumed to work with minimal issues.

---
+++ examples/configurable_widget/macos/ConfigurableWidget/ConfigurableWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ConfigurableWidgetBundle.swift
+//  ConfigurableWidget
+//
+//  Created by Devin Grischow on 9/24/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ConfigurableWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ConfigurableWidget()
+    }
+}
